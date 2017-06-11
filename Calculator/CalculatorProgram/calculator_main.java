de2f1d28b011834ca455648ff6aac4f7c7e9af71
Chris Martin <chrismartin16cm@gmail.com>
2017-06-11 16:42:50 -0400

    Update calculator_main.java

--- Calculator/CalculatorProgram/calculator_main.java
+++ Calculator/CalculatorProgram/calculator_main.java
@@ -1,1 +1,19 @@
+package calculatorprogram;
+public class calculator_main extends calculator_ui{
+    
+    
+    public static void main(String[] args) {
+        calculator_ui n = new calculator_ui();
+        n.ui();
+        
+        
+        
+    }
+    
+    
+    
+    
 
+
+
+}
