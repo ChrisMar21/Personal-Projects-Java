37b6a088df772c61e4a205a708521df78827f1e5
Chris Martin <chrismartin16cm@gmail.com>
2020-05-27 15:53:14 -0400

    Add files via upload
    Reversing Integers in Java

---
+++ Reversing Integers in Java (YouTube)/src/NumberReverser.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class NumberReverser{
+   //int number;
+    NumberReverser(int number){
+        //this.number = number;
+    }
+    public void reversingNumber(){
+        //String num = String.valueOf(this.number);
+        String reversedNumber = "";
+        
+        //for(int i = num.length()-1;i >= 0;i--){
+            
+        
+        //}
+     //System.out.println("Number : " + this.number);
+     System.out.println("Number Reversed: " + Integer.parseInt(reversedNumber));
+    }
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Please enter a number that you would like reversed: ");
+        //int num = input.nextInt();
+        //NumberReverser nr = new NumberReverser(num);
+        //nr.reversingNumber();   
+    }
+}
